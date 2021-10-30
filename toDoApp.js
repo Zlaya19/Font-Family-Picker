bf62e5f041c76c5117d38b369e7e8e74f0e79a9d
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-10-30 21:03:16 +0200

    appending logic

--- toDoApp.js
+++ toDoApp.js
@@ -2,14 +2,44 @@ const btn = document.getElementById('btn');
 const area = document.getElementById('area');
 const output = document.getElementById('output');
 
+let updatedStatus = false;
+
 btn.addEventListener('click', () => {
-    const area = document.getElementById('area');
-    const output = document.getElementById('output');
-    let paragraph = document.createElement('h2');
-    paragraph.innerText = area.value;
-    paragraph.classList.add('class_list');
-    output.appendChild(paragraph);
-    output.innertext = area.value;
-    console.log(paragraph);
-    /* output.style.dislay = 'block'; */
+
+        checkStatus();
+
+        function checkStatus(){
+                if(updatedStatus === false){
+                        appendParagraph();
+                }else{
+                        output.innerText = '';
+                        updatedStatus = false;
+                }
+        }
+
+        function appendParagraph(){
+
+                const radioBtn = document.querySelector('input[name="radioBtn"]:checked').value;
+
+                output.style.opacity = 1;
+                let paragraph = document.createElement('h2');
+                paragraph.innerText = area.value;
+                paragraph.classList.add('class_list');         
+
+        if(radioBtn === 'font1'){
+                paragraph.style.fontFamily = 'verdana';
+        }
+        if(radioBtn === 'font2'){
+                paragraph.style.fontFamily = 'arial';
+        }
+        if(radioBtn === 'font3'){
+                paragraph.style.fontFamily = 'calibri';
+        }
+
+        output.appendChild(paragraph);
+        updatedStatus = true;
+        } 
+        
 });
+
+       
